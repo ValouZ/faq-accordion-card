05c71ee35002fca7825dc4a4e661541dbeb770a9
ValouZ <debray.va@gmail.com>
2020-12-19 00:59:53 +0100

    mobile version working

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 const group = document.getElementsByClassName("group");
+const questionDiv = document.getElementsByClassName("question-div");
+const questions = document.getElementsByClassName("question");
+const answers = document.getElementsByClassName("answer");
+console.log(answers)
+for (let i = 0; i < questionDiv.length; i++) {
+    questionDiv[i].addEventListener("click", displayName);
+}
+
+
+function displayName() {
+    let answer = this.parentNode.getElementsByClassName("answer")[0];
+    let question = this.getElementsByClassName("question")[0];
+
+    // Add / Remove font-weight bold to the questions
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].classList.remove("selected");
+    }
+    question.classList.add("selected");
+
+    // Display the answer
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].classList.remove("display-answer");
+        questions[i].parentNode.style.paddingBottom = "19px";
+    }
+    question.parentNode.style.paddingBottom = "0px";
+    answer.classList.add("display-answer");
+}
