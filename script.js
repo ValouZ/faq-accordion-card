c00a741682d88af6fe61d143ab2ba292d3d9bd13
ValouZ <debray.va@gmail.com>
2020-12-19 01:14:36 +0100

    we can now close the selected question

--- script.js
+++ script.js
@@ -11,18 +11,42 @@ for (let i = 0; i < questionDiv.length; i++) {
 function displayName() {
     let answer = this.parentNode.getElementsByClassName("answer")[0];
     let question = this.getElementsByClassName("question")[0];
+    let status = alreadySelected(question);
 
-    // Add / Remove font-weight bold to the questions
-    for (let i = 0; i < questions.length; i++) {
-        questions[i].classList.remove("selected");
-    }
-    question.classList.add("selected");
+    // If we are clicking the question that already has the "selected" class, 
+    // we remove it
+    if (status) {
+        // Remove font-weight bold to the question and add the standard padding
+        question.classList.remove("selected");
+        question.parentNode.style.paddingBottom = "19px";
 
-    // Display the answer
-    for (let i = 0; i < answers.length; i++) {
-        answers[i].classList.remove("display-answer");
-        questions[i].parentNode.style.paddingBottom = "19px";
+        // Remove the answer
+        answer.classList.remove("display-answer");
     }
-    question.parentNode.style.paddingBottom = "0px";
-    answer.classList.add("display-answer");
+    // In the other case, we remove all the "selected" class and we add it
+    // to the clicked one
+    else {
+        for (let i = 0; i < questions.length; i++) {
+            // Remove font-weight bold to questions and add the standard padding
+            questions[i].classList.remove("selected");
+            questions[i].parentNode.style.paddingBottom = "19px";
+
+            // Remove the answer
+            answers[i].classList.remove("display-answer");
+        }
+        // Add font-weight bold to the question and remove the standard padding
+        question.classList.add("selected");
+        question.parentNode.style.paddingBottom = "0px";
+
+        // Display the answer
+        answer.classList.add("display-answer");
+    }
+}
+
+// Return -true- if the question we clicked is already displayed (had "selected" class)
+// -false- if not
+function alreadySelected(question) {
+    if (question.classList.contains("selected"))
+        return true;
+    return false;
 }
